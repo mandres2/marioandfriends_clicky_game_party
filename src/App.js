e7d4e6610b6cb5716eeaed45c100a95022e08c57
Micah Andres <mandres1@uw.edu>
2019-11-06 11:48:36 -0800

    updating touch files in components, set code in Card folder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './components/Card';
+import Header from './components/Header';
+import Wrapper from './components/Wrapper';
+import cards from './cards.json';
+import './App.css';
+
+class App extends Component {
+
+}
+
+export default App;
