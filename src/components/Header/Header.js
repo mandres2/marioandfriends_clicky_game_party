8eafee377dc653a84219d78903fd536093619e20
Micah Andres <mandres1@uw.edu>
2019-11-07 18:25:12 -0800

    fixed front-end positioning of header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,12 +7,15 @@ const Header = props => (
   <div className="header">
     <div className="title">
       {props.children}</div>
-      <br></br>
-      div className="scores">
+      <div className="scores">
       Score: {props.score}
       <br></br>
       Highscore: {props.highscore}
-    <br></br>
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
     <div className="rules">Click on an image to earn points, but don't click on any more than once!</div>
   </div>
 );
