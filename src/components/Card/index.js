599aa4f0b3e773c76fb76dce3611ed1c0df2c660
Micah Andres <mandres1@uw.edu>
2019-11-11 16:29:24 -0800

    Updates

--- src/components/Card/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from './Card';
-// exporting default from Card folder
--- src/components/Wrapper/Wrapper.js
+++
@@ -1,9 +0,0 @@
-// React declaration of dependency
-import React from 'react';
-// Import the file Wrapper.css
-import './Wrapper.css';
-
-const Wrapper = props => <div className="wrapper">{props.children}</div>
-
-// Export the object so that it can be used
-export default Wrapper;
